bdcb6407258b6dfa2bdf90c49abc571c1cdc91a1
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-09 18:14:45 +0300

    add_targetCoords

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # astropy-1.0.6
 
 from astropy import units as u
-from astropy.coordinates import EarthLocation
+from astropy.coordinates import EarthLocation, SkyCoord
 from astropy.time import Time
 
 # TODO: change height value
@@ -10,3 +10,14 @@ print 'Station (Armavir) coordinates: ' + str(stationCoords)
 
 # TODO: use timezone shift
 time = Time('2015-11-09T00:00:00.000Z')
+
+# TODO: check Armavir station viewing angles/distances
+# TODO: check distance param (alt: 45 deg; 1250 / Math.sqrt(2) = 884)
+targetCoords = SkyCoord(
+    az=45*u.deg,
+    alt=45*u.deg,
+    distance=1250*u.km,
+    location=stationCoords,
+    obstime=time,
+    frame='altaz'
+)
