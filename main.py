eb6cff59937c3e1176559e48d47f755dac5026cb
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-09 18:04:02 +0300

    add_time_using

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 
 from astropy import units as u
 from astropy.coordinates import EarthLocation
+from astropy.time import Time
 
 # TODO: change height value
 stationCoords = EarthLocation(lat=44.93*u.deg, lon=40.98*u.deg, height=0*u.km)
 print 'Station (Armavir) coordinates: ' + str(stationCoords)
+
+# TODO: use timezone shift
+time = Time('2015-11-09T00:00:00.000Z')
