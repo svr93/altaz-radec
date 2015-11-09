bda003907bc74a1f2ca48dd0a309d0ae1262e1a4
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir.local>
2015-11-09 18:44:19 +0300

    add_angular_distance

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from astropy import units as u
 from astropy.coordinates import EarthLocation, SkyCoord
 from astropy.time import Time
+import numpy as np
 
 stationLat=44.93*u.deg # dec
 stationLng=40.98*u.deg # ra
@@ -12,6 +13,8 @@ print 'Station (Armavir) coordinates: ' + str(stationCoords)
 
 # TODO: use timezone shift
 time = Time('2015-11-09T00:00:00.000Z')
+
+distance=1250*u.km
 
 # TODO: check Armavir station viewing angles/distances
 # TODO: check distance param (alt: 45 deg; 1250 / Math.sqrt(2) = 884)
@@ -30,3 +33,12 @@ print 'Target coordinates in ICRS system: ' + str(targetCoordsICRS)
 
 targetLat=targetCoordsICRS.data.lat # dec
 targetLng=targetCoordsICRS.data.lon # ra
+
+# http://spiff.rit.edu/classes/phys373/lectures/radec/radec.html
+# cos(y) = sin(dec1)sin(dec2) + cos(dec1)cos(dec2)cos(ra1 - ra2)
+
+angularDistCos=np.sin(stationLat) * np.sin(targetLat) + \
+    np.cos(stationLat) * np.cos(targetLat) * np.cos(stationLng - targetLng)   
+
+angularDist = np.arccos(angularDistCos)
+print 'Angular distance between station and target: ' + str(angularDist)
